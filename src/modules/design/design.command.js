460f635c8c55303b761d87db7ff9798424900eaf
austeridez <sarapitucoh@gmail.com>
2026-01-05 18:18:42 -0400

    Atualizar o design.command.js

--- src/modules/design/design.command.js
+++ src/modules/design/design.command.js
@@ -3,8 +3,8 @@ module.exports = {
 
   async execute(message, client) {
     // IDs permitidos (ajuste depois)
-    const OWNERS = ['SEU_ID_AQUI'];
-    const DEVS = ['SEU_ID_AQUI'];
+    const OWNERS = ['1430469417149661254'];
+    const DEVS = ['1431872681778745496'];
 
     if (
       !OWNERS.includes(message.author.id) &&
