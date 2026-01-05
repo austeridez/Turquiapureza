a84124172266bc7dafb2e3ee75d25c9d48965b08
austeridez <sarapitucoh@gmail.com>
2026-01-05 18:03:52 -0400

    Criar src/modules/design/design.command.js

---
+++ src/modules/design/design.command.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'embeddesign',
+
+  async execute(message, client) {
+    // apenas teste inicial
+    await message.reply('🎨 Comando embeddesign reconhecido.');
+  }
+};
