92cd27f75d80973ef16114ee53d35ddf28867240
austeridez <sarapitucoh@gmail.com>
2026-01-05 18:39:26 -0400

    Atualizar o messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,16 +1,5 @@
 module.exports = async (message, client) => {
   if (message.author.bot) return;
-  if (!message.content.startsWith('!')) return;
 
-  const args = message.content.slice(1).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
-
-  const command = client.prefixCommands.get(commandName);
-  if (!command) return;
-
-  try {
-    await command.execute(message, client, args);
-  } catch (err) {
-    console.error(err);
-  }
+  console.log('📩 Mensagem recebida:', message.content);
 };
