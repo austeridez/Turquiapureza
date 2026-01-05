122cf41f97ac33b0dc9456fb09ca44f10bd2f517
austeridez <sarapitucoh@gmail.com>
2026-01-05 19:23:20 -0400

    Atualizar o design.module.js

--- src/modules/design/design.module.js
+++ src/modules/design/design.module.js
@@ -4,6 +4,13 @@ module.exports = (client) => {
   const command = require('./design.command');
   client.prefixCommands.set(command.name, command);
 
+  // botões
+  const bannerButton = require('./buttons/banner.button');
+  const barrinhaButton = require('./buttons/barrinha.button');
+
+  client.interactions.set(bannerButton.customId, bannerButton);
+  client.interactions.set(barrinhaButton.customId, barrinhaButton);
+
   console.log(
     '📦 Comandos registrados no DESIGN:',
     [...client.prefixCommands.keys()]
