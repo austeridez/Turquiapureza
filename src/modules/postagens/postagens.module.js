df8928b5cb5aed142e0c5162f5e5925076b2e5a7
austeridez <sarapitucoh@gmail.com>
2026-01-11 14:41:24 -0400

    Atualizar o postagens.module.js

--- src/modules/postagens/postagens.module.js
+++ src/modules/postagens/postagens.module.js
@@ -1,14 +1,23 @@
 module.exports = (client) => {
   console.log('🗓️ Módulo POSTAGENS carregado');
 
-  const postagens = require('./postagens.command');
-  client.prefixCommands.set(postagens.name, postagens);
+  client.prefixCommands.set(
+    'postagenssemanal',
+    require('./postagens.command')
+  );
 
   const editar = require('./editarcronograma.command');
   client.prefixCommands.set(editar.name, editar);
 
-  const select = require('./postagens.select');
-  client.interactions.set(select.customId, select);
+  client.interactions.set(
+    'postagens_menu',
+    require('./postagens.select')
+  );
+
+  const buttons = require('./editarcronograma.buttons');
+  for (const id of buttons.customId) {
+    client.interactions.set(id, buttons);
+  }
 
   const listener = require('./editarcronograma.listener');
   client.on('messageCreate', (msg) => listener(msg, client));
